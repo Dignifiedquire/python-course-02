d9c2d78bce9d92647a47b08ec347c245626ab03c
dignifiedquire <dignifiedquire@gmail.com>
2015-02-11 12:48:02 +0100

    First round of content

--- 01/01.py
+++
@@ -1,5 +0,0 @@
-# Set the list of names
-names = ["Bob", "Jack", "Sonya", "Will", "Joe"]
-
-
-# Print all the names to the console
---
+++ 06-dictionaries/task-01c.py
@@ -0,0 +1,14 @@
+s = "I want milk!"
+
+translation = ""
+
+for w in s.split():
+
+    print(w)
+
+    if w in d_e.keys():
+        translation+=d_e[w]+" "
+    else:
+        translation += w
+
+print(" ".join(translation))
